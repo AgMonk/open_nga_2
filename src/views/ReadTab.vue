a9fb49f3df6510b061ba5aa542a6efc1335e70c5
ginstone <hjg719@139.com>
2021-12-23 11:30:25 +0800

    回复列表 - 请求数据、设置标题

--- src/views/ReadTab.vue
+++ src/views/ReadTab.vue
@@ -15,13 +15,37 @@
 </template>
 
 <script>
+import {mapActions} from "vuex";
+import {setTitle} from "@/assets/request/ProjectUtils";
+
 export default {
   name: "ReadTab",
   data() {
-    return {}
+    return {
+      pageData: {
+        total: 100,
+        currentPage: 1,
+        pageSize: 20,
+      },
+      replies:[],
+    }
   },
-  methods: {},
+  methods: {
+    ...mapActions("read", [`getReplies`]),
+    async get(force) {
+      const {pid, tid, page, authorid} = Object.assign({}, this.$route.query, this.$route.params)
+      const res = await this.getReplies({pid, tid, page, authorid, force})
+      const data = res.data;
+      console.log(data);
+      setTitle(data.forum.name)
+
+      this.pageData = data.pageData;
+      this.replies = data.replies;
+    }
+  },
   mounted() {
+    setTitle(this.$route.name)
+    this.get(false)
   },
   watch: {},
   props: {},

e1c945068ac42336d8b4bf2b94306f818b320816
ginstone <hjg719@139.com>
2021-12-27 10:27:50 +0800

    主题链接组件

---
+++ src/components/nga/nga-thread-link.vue
@@ -0,0 +1,39 @@
+<template>
+  <my-router-link v-if="isType(data.mirror,'版面')" :to="`/thread/f/${data.mirror.fid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
+    {{data.subject}}
+  </my-router-link>
+  <my-router-link v-else-if="isType(data.mirror,'合集')" :to="`/thread/s/${data.tid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
+    {{data.subject}}
+  </my-router-link>
+  <my-router-link v-else :to="`/read/t/${data.tid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
+    {{data.subject}}
+  </my-router-link>
+</template>
+
+<script>
+import MyRouterLink from "@/components/my/my-router-link";
+export default {
+  name: "nga-thread-link",
+  components: {MyRouterLink},
+  data() {
+    return {}
+  },
+  methods: {
+    //判断是否为子主题
+    isType(mirror,type) {
+      return mirror && mirror.type && (mirror.type===type || type.includes(mirror.type));
+    },
+  },
+  mounted() {
+  },
+  watch: {},
+  props: {
+    data:{type:Object,required:true}
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>

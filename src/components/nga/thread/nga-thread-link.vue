15826f5c25a9d173df254460f3f5a81eb24d859b
ginstone <hjg719@139.com>
2021-12-31 14:12:17 +0800

    主题链接逻辑简化

--- src/components/nga/thread/nga-thread-link.vue
+++ src/components/nga/thread/nga-thread-link.vue
@@ -1,17 +1,12 @@
 <template>
-  <my-router-link v-if="isType(data.mirror,'版面')" :to="`/thread/f/${data.mirror.fid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
-    {{data.subject}}
-  </my-router-link>
-  <my-router-link v-else-if="isType(data.mirror,'合集')" :to="`/thread/s/${data.tid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
-    {{data.subject}}
-  </my-router-link>
-  <my-router-link v-else :to="`/read/t/${data.tid}/1`" :style="data.titleFont?data.titleFont.style:undefined">
-    {{data.subject}}
+  <my-router-link :style="data.titleFont?data.titleFont.style:undefined" :to="getRoute(data)">
+    {{ text ? text : data.subject }}
   </my-router-link>
 </template>
 
 <script>
 import MyRouterLink from "@/components/my/my-router-link";
+
 export default {
   name: "nga-thread-link",
   components: {MyRouterLink},
@@ -20,15 +15,24 @@ export default {
   },
   methods: {
     //判断是否为子主题
-    isType(mirror,type) {
-      return mirror && mirror.type && (mirror.type===type || type.includes(mirror.type));
+    isType(mirror, type) {
+      return mirror && mirror.type && (mirror.type === type || type.includes(mirror.type));
+    },
+    getRoute(data) {
+      if (this.isType(data.mirror, '版面')) {
+        return {name: '浏览版面主题', params: {fid: data.mirror.fid, page: 1}}
+      } else if (this.isType(data.mirror, '合集')) {
+        return {name: '浏览合集主题', params: {stid: data.tid, page: 1}}
+      }
+      return {name: '回复列表', params: {tid: data.tid, page: 1}}
     },
   },
   mounted() {
   },
   watch: {},
   props: {
-    data:{type:Object,required:true}
+    data: {type: Object, required: true},
+    text: {},
   },
 }
 

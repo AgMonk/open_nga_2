73f747474c8fb2724ca7218fec16c8bb63cccfa5
ginstone <hjg719@139.com>
2022-01-05 14:03:38 +0800

    回复正文 - 暂时移除签名区

--- src/components/nga/read/nga-read-table.vue
+++ src/components/nga/read/nga-read-table.vue
@@ -72,13 +72,13 @@
                 <el-divider content-position="left">附件区({{ row.attachs.length }})</el-divider>
                 <nga-attach-tag v-for="img in row.attachs" :data="img"/>
               </div>
-              <div v-if="users[row.authorid] && users[row.authorid].signature" style="text-align: left">
-                <el-collapse>
-                  <el-collapse-item title="[签名区]">
-                    <nga-signature :uid="row.authorid"/>
-                  </el-collapse-item>
-                </el-collapse>
-              </div>
+              <!--              <div v-if="users[row.authorid] && users[row.authorid].signature" style="text-align: left">-->
+              <!--                <el-collapse>-->
+              <!--                  <el-collapse-item title="[签名区]">-->
+              <!--                    <nga-signature :uid="row.authorid"/>-->
+              <!--                  </el-collapse-item>-->
+              <!--                </el-collapse>-->
+              <!--              </div>-->
             </el-card>
           </el-col>
 

ff9d56c13f59f41d0648e2d037d9bde45053212d
ginstone <hjg719@139.com>
2022-01-14 16:32:13 +0800

    回复正文 - 计算dice标签结果

--- src/components/nga/read/nga-dice-tag.vue
+++ src/components/nga/read/nga-dice-tag.vue
@@ -1,22 +1,47 @@
 <template>
-  <el-tag effect="dark" type="warning">{{ text }}</el-tag>
-
+  <div>
+    <el-tooltip :content="content" effect="light">
+      <el-tag effect="dark" type="warning">{{ text }}</el-tag>
+    </el-tooltip>
+  </div>
 </template>
 
 <script>
+import {rnd} from "@/assets/nga/nga-dice";
+
 export default {
   name: "nga-dice-tag",
   data() {
     return {
       text: "",
+      content: "",
     }
   },
   computed: {},
   methods: {
     update(data) {
-      console.log(this.seed)
-
-      this.text = data;
+      let tempSeed = this.seed.seed;
+      const group = data.split('+')
+      const pattern = /(\d*)d(\d+)/
+      const results = []
+      let sum = 0;
+      group.forEach(item => {
+        const match = pattern.exec(item)
+        if (match) {
+          const count = parseInt(match[1] ? match[1] : 1);
+          const range = parseInt(match[2])
+          for (let i = 0; i < count; i++) {
+            const {seed, result} = rnd(tempSeed)
+            const r = Math.floor(result * range + 1)
+            sum += r;
+            tempSeed = seed;
+            results.push({range, result: r})
+          }
+        }
+      })
+      this.seed.seed = tempSeed
+      this.content = results.map(i => `d${i.range}(${i.result})`).join('+') + ' = ' + sum
+      this.text = `Roll:${data} 结果：${sum}`;
     }
   },
   mounted() {
--- src/store/history.js
+++ src/store/history.js
@@ -24,20 +24,20 @@ export default {
         },
         addHistoryForum(state, {fid, name, recommend}) {
             const n = recommend ? `${name}(精华区)` : name;
-            slf4j(`添加版面浏览记录 fid:${fid} name:${n}`)
+            // slf4j(`添加版面浏览记录 fid:${fid} name:${n}`)
             state.forums = state.forums.filter(i => i.name !== n)
             state.forums.splice(0, 0, {fid: parseInt(fid), name: n, recommend})
             //    保存版面浏览记录
             putCache("ForumHistory", state.forums)
         },
         addHistoryThread(state, {tid, name}) {
-            slf4j(`添加主题浏览记录 tid:${tid} name:${name}`)
-            state.threads = state.threads.filter(i => i.tid !== parseInt(tid))
+            // slf4j(`添加主题浏览记录 tid:${tid} name:${name}`)
+            state.threads = state.threads.filter(i => parseInt(i.tid) !== parseInt(tid))
             state.threads.splice(0, 0, {tid: parseInt(tid), name})
         },
         addHistorySet(state, {stid, name, forumName}) {
             const n = `${forumName}>${name}`
-            slf4j(`添加合集浏览记录 stid:${stid} name:${n}`)
+            // slf4j(`添加合集浏览记录 stid:${stid} name:${n}`)
             state.sets = state.sets.filter(i => i.name !== n)
             state.sets.splice(0, 0, {stid: parseInt(stid), name: n})
 

c70a165f5280328735e86cd17a731c57f8b81a3e
ginstone <hjg719@139.com>
2022-01-18 13:52:03 +0800

    图片 - 宽度超过400时添加换行

--- src/components/nga/content/nga-content-img.vue
+++ src/components/nga/content/nga-content-img.vue
@@ -1,22 +1,25 @@
 <template>
-  <el-tooltip :show-after="500" effect="light">
-    <template #content>
-      <h3>点击打开原图</h3>
-      <div v-if="src && isNgaImg(src)">
-        <el-divider content-position="left">复制</el-divider>
-        <my-copy-button :copy-text="`[img]./${src.substring(src.indexOf('mon_'))}[/img]`" text="BbsCode" />
-        <my-copy-button :copy-text="`https://img.nga.178.com${link}`" text="地址" />
-      </div>
-      <div v-if="link">
-        <el-divider content-position="left">打开</el-divider>
-        <el-button size="small" type="primary" @click="open(link)">打开原图</el-button>
-        <el-button size="small" type="primary" @click="download(link)">下载原图</el-button>
-      </div>
-    </template>
-    <span>
-      <el-image v-if="show" :preview-src-list="srcList" :src="link" append-to-body hide-on-click-modal />
-    </span>
-  </el-tooltip>
+  <span>
+    <el-tooltip :show-after="500" effect="light">
+      <template #content>
+        <h3>点击打开原图</h3>
+        <div v-if="src && isNgaImg(src)">
+          <el-divider content-position="left">复制</el-divider>
+          <my-copy-button :copy-text="`[img]./${src.substring(src.indexOf('mon_'))}[/img]`" text="BbsCode" />
+          <my-copy-button :copy-text="`https://img.nga.178.com${link}`" text="地址" />
+        </div>
+        <div v-if="link">
+          <el-divider content-position="left">打开</el-divider>
+          <el-button size="small" type="primary" @click="open(link)">打开原图</el-button>
+          <el-button size="small" type="primary" @click="download(link)">下载原图</el-button>
+        </div>
+      </template>
+      <span>
+        <el-image v-if="show" :id="uuid" :preview-src-list="srcList" :src="link" append-to-body hide-on-click-modal />
+      </span>
+    </el-tooltip>
+    <br v-if="br" />
+  </span>
 </template>
 
 <script>
@@ -31,6 +34,8 @@ export default {
       link: "",
       show: false,
       srcList: [],
+      uuid: "",
+      br: false,
     }
   },
   methods: {
@@ -52,11 +57,18 @@ export default {
       if (this.isNgaImg(src)) {
         this.link = '/attachments/' + src.substring(src.indexOf("mon_"))
         this.srcList = ['/attachments/' + src.replace(".medium.jpg", "").replace(/\.thumb.*jpg/, "")]
-        this.show = true;
       } else {
         this.link = '/proxy/' + encodeURI(src.replace('https://', '').replace('http://', ''))
-        this.show = true;
       }
+      this.uuid = 'img' + Math.floor(233280 * Math.random())
+      this.show = true;
+
+      setTimeout(() => {
+        const img = document.getElementById(this.uuid);
+        if (img && img.width > 400) {
+          this.br = true;
+        }
+      }, 1000)
     },
   },
   mounted() {
